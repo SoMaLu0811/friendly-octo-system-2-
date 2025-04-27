0d93a70ac868dad7da080d2d6554097d42378e79
Marco Aurelio Bresciani Weber <121580696+SoMaLu0811@users.noreply.github.com>
2025-04-27 18:32:00 -0300

    Update account.py

--- meuapp/models/account.py
+++ meuapp/models/account.py
@@ -1,7 +1,7 @@
 from meuapp.models.base import basemodel
 from django.contrib.auth.models import User
 from django.db import models
-class account(basemodel):
+class Account(basemodel):
     owner=models.ManyToManyField(User, verbose_name='Conta')
     description=models.CharField(max_length=200, help_text='Descriça da sua conta')
     number=models.CharField(max_length=200,help_text='Insira o número da sua conta')
