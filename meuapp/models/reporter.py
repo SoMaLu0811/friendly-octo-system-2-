cf461e1451ad39991ee2fac786eb268f029734fe
SoMaLu0811 <quito110608@gmail.com>
2025-04-27 15:24:36 -0300

    Adição de novos características na classe Reporter.

--- meuapp/models/reporter.py
+++ meuapp/models/reporter.py
@@ -4,6 +4,11 @@ class Reporter(basemodel):
     name=models.CharField(max_length=100,verbose_name='Nome Completo',help_text="Insira seu nome completo")
     email=models.EmailField(verbose_name='E-mail',help_text="Insira seu E-mail")
     cpf=models.CharField(max_length=11,verbose_name='CPF',help_text="Insira seu número de cpf")
+    idade=models.CharField(max_length=3,default=0,verbose_name='Idade',help_text='Digite sua idade:')
+    nacionalidade=models.CharField(max_length=100, default='Não especificado',verbose_name='Nacionalidade',help_text='Insira aqui sua nacionalidade')
+    
+    descrição=models.CharField(max_length=200,default='',help_text='Insira a descrição do escritor')
+    
     class Meta:
         abstract=False
     def __str__(self):
