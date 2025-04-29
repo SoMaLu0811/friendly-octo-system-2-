1c7e03544bc0effb74f9ae56493de26a5e785f46
SoMaLu0811 <quito110608@gmail.com>
2025-04-29 07:20:00 -0300

    Registro de novos abas para o admin.py, criação do modelo Magazine, e nova migração de modelos para o servidor

--- meuapp/admin.py
+++ meuapp/admin.py
@@ -4,8 +4,11 @@ from meuapp.models.person import Person
 from meuapp.models.passport import Passport
 from meuapp.models.reporter import Reporter
 from meuapp.models.article import Article
+from meuapp.models.magazine import Magazine
 admin.site.register(account)
 admin.site.register(Person)
 admin.site.register(Passport)
 admin.site.register(Reporter)
 admin.site.register(Article)
+admin.site.register(Magazine)
+
--- meuapp/models/__init__.py
+++ meuapp/models/__init__.py
@@ -3,4 +3,5 @@ from .account import*
 from .person import*
 from .passport import*
 from .reporter import*
+from .magazine import*
 from .article import*
